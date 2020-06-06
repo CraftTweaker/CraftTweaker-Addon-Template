3bd9ba8f316c988a89e2fecba37fa398a27269cd
kindlich <kindlich@gmx.net>
2020-06-06 16:05:58 +0200

    Missed classes lol

--- src/main/java/com/blamejared/some_generic_addon/SomeGenericAddon.java
+++ src/main/java/com/blamejared/some_generic_addon/SomeGenericAddon.java
@@ -1,2 +1,6 @@
-package com.blamejared.some_generic_addon;public class SomeGenericAddon {
-}
+package com.blamejared.some_generic_addon;
+
+import net.minecraftforge.fml.common.*;
+
+@Mod("some_generic_addon")
+public class SomeGenericAddon {}
--- src/main/java/com/blamejared/some_generic_addon/compat/crafttweaker/MyStaticIntegrationClass.java
+++ src/main/java/com/blamejared/some_generic_addon/compat/crafttweaker/MyStaticIntegrationClass.java
@@ -1,2 +1,27 @@
-package com.blamejared.some_generic_addon.compat.crafttweaker;public class MyStaticIntegrationClass {
+package com.blamejared.some_generic_addon.compat.crafttweaker;
+
+import com.blamejared.crafttweaker.api.*;
+import com.blamejared.crafttweaker.api.annotations.*;
+import com.blamejared.crafttweaker.api.item.*;
+import com.blamejared.crafttweaker_annotations.annotations.*;
+import org.openzen.zencode.java.*;
+
+/**
+ * A class with static members that allow us to do stuff!
+ */
+@ZenRegister
+@ZenCodeType.Name("mods.some_generic_addon.MyStaticIntegrationClass")
+@Document("mods/some_generic_addon/MyStaticIntegrationClass")
+public class MyStaticIntegrationClass {
+    
+    /**
+     * Prints the given ingredient to the console, yay!
+     *
+     * @param ingredient The ingredient to be printed
+     * @docParam ingredient <item:minecraft:iron_ingot>
+     */
+    @ZenCodeType.Method
+    public static void doPrint(IIngredient ingredient) {
+        CraftTweakerAPI.logInfo("Printing '%s'", ingredient.getCommandString());
+    }
 }
